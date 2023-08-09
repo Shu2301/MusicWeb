177d556ef6eff65f7f383f66363472109d4c33e8
Trung Nguyễn <91687364+Shu2301@users.noreply.github.com>
2023-08-10 02:05:57 +0700

    update for smartphone

--- app.js
+++ app.js
@@ -337,3 +337,18 @@ pop_art_right.addEventListener('click', () => {
 pop_art_left.addEventListener('click', () => {
     Artists_bx.scrollLeft -= 330;
 })
+
+let menu_list_active_button = document.getElementById('menu_list_active_button');
+let menu_side = document.getElementsByClassName('menu_side')[0];
+
+menu_list_active_button.addEventListener('click', () => {
+    menu_side.style.transform = "unset";
+    menu_list_active_button.style.opacity = 0;
+})
+
+let song_side = document.getElementsByClassName('song_side')[0];
+
+song_side.addEventListener('click', () => {
+    menu_side.style.transform = "translateX(-100%)";
+    menu_list_active_button.style.opacity = 1;
+})
